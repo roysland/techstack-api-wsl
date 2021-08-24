7378278093243023342a117a2e04987528c1edb7
Øyvind Røysland <roysland@gmail.com>
2021-08-24 19:02:18 +0200

    first

---
+++ helpers/geo.py
@@ -0,0 +1,14 @@
+import numpy as np
+"""
+The Haversine formula determines the great-circle distance between two points on a sphere 
+given their longitudes and latitudes.
+"""
+def haversine_distance(lat1, lon1, lat2, lon2):
+   r = 6371
+   phi1 = np.radians(lat1)
+   phi2 = np.radians(lat2)
+   delta_phi = np.radians(lat2 - lat1)
+   delta_lambda = np.radians(lon2 - lon1)
+   a = np.sin(delta_phi / 2)**2 + np.cos(phi1) * np.cos(phi2) *   np.sin(delta_lambda / 2)**2
+   res = r * (2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a)))
+   return np.round(res, 2)
